033d98d0ac5e5b313222ba9123b39e8c194b547b
pipboy3003 <rndsmqjndr@privaterelay.appleid.com>
2025-12-18 08:16:27 +0100

    Update world_gen.js

--- world_gen.js
+++ world_gen.js
@@ -1,51 +1,5 @@
 const WorldGen = {
-    // FALLOUT 3 INSPIRED BIOME DEFINITIONS
-    biomes: {
-        'wasteland': { 
-            ground: '.', 
-            features: [
-                { char: 'o', prob: 0.04, solid: true },  // Felsen
-                { char: 'x', prob: 0.03, solid: false }, // Toter Strauch
-                { char: 't', prob: 0.01, solid: true }   // Einzelner toter Baum
-            ],
-            water: 0.02, mountain: 0.05 
-        },
-        'jungle': { // "Oasis" Style
-            ground: ',', 
-            features: [
-                { char: 'T', prob: 0.10, solid: true },  // Großer Baum
-                { char: 't', prob: 0.25, solid: true },  // Kleiner Baum
-                { char: '"', prob: 0.15, solid: false }  // Hohes Gras
-            ],
-            water: 0.10, mountain: 0.1 
-        },
-        'desert': { // "The Pitt" / Ashlands Style
-            ground: '_', 
-            features: [
-                { char: 'o', prob: 0.02, solid: true },  // Kleine Steine
-                { char: 'Y', prob: 0.03, solid: true }   // Verdorrte Kakteen/Holz
-            ],
-            water: 0.01, mountain: 0.15 
-        },
-        'city': { // "D.C. Ruins" Style
-            ground: '=', 
-            features: [
-                { char: '#', prob: 0.12, solid: true },  // Mauerreste
-                { char: '+', prob: 0.08, solid: false }, // Schutt/Geröll
-                { char: 'o', prob: 0.03, solid: true }   // Betonbrocken
-            ],
-            water: 0.0, mountain: 0.0 
-        },
-        'swamp': { // "Point Lookout" Style
-            ground: ';', 
-            features: [
-                { char: '~', prob: 0.15, solid: false }, // Wasserlache (verstrahlt?)
-                { char: 'x', prob: 0.10, solid: false }, // Schilf
-                { char: 't', prob: 0.08, solid: true }   // Modrige Bäume
-            ],
-            water: 0.05, mountain: 0.0
-        }
-    },
+    // Biomes werden jetzt aus GameData (data.js) geladen
     
     _seed: 12345,
 
@@ -62,31 +16,24 @@ const WorldGen = {
     createSector: function(width, height, biomeType, poiList) {
         let map = Array(height).fill().map(() => Array(width).fill('.'));
         
-        // Zufälliges Biom wählen, falls nicht spezifiziert, oder "swamp" hinzufügen
-        if (!this.biomes[biomeType]) {
-            const types = Object.keys(this.biomes);
-            // Nutze Seed für Biom-Wahl wenn möglich, hier vereinfacht basierend auf Input
+        // Zugriff auf externe Daten (GameData)
+        if (typeof GameData === 'undefined' || !GameData.biomes[biomeType]) {
             biomeType = 'wasteland'; 
         }
         
-        const conf = this.biomes[biomeType];
+        const conf = GameData.biomes[biomeType];
 
         // 1. BASIS TERRAIN & FEATURES
         for(let y = 0; y < height; y++) {
             for(let x = 0; x < width; x++) {
                 const r = this.rand();
-                
-                // Basis
                 map[y][x] = conf.ground;
 
-                // Große Features (Wasser/Berge)
                 if(r < conf.water) {
                     map[y][x] = 'W';
                 } else if(r < conf.water + conf.mountain) {
                     map[y][x] = 'M';
                 } else {
-                    // Detail Features (Bäume, Steine, etc.)
-                    // Wir würfeln nochmal für Details, damit sie nicht nur an Bergen kleben
                     const d = this.rand();
                     let currentProb = 0;
                     for(let feat of conf.features) {
@@ -105,8 +52,6 @@ const WorldGen = {
             poiList.forEach(poi => {
                 if(poi.x >= 0 && poi.x < width && poi.y >= 0 && poi.y < height) {
                     map[poi.y][poi.x] = poi.type;
-                    
-                    // Schutzzone: Boden um POI säubern
                     for(let dy=-2; dy<=2; dy++) {
                         for(let dx=-2; dx<=2; dx++) {
                             const ny = poi.y+dy, nx = poi.x+dx;
