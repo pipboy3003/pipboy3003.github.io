3e6c528617cff762274d8a88b786c4f6e78c87b0
pipboy3003 <rndsmqjndr@privaterelay.appleid.com>
2026-07-15 09:07:14 +0200

    Update firebase-config.js

--- school03/firebase-config.js
+++ school03/firebase-config.js
@@ -1,13 +1,13 @@
-// Firebase-Konfiguration – bitte mit echten Projektdaten aus der Firebase Console ersetzen
-// (Projekteinstellungen -> Meine Apps -> Web-App -> SDK-Konfiguration kopieren).
 const firebaseConfig = {
-  apiKey: "DEIN_API_KEY",
-  authDomain: "DEIN_PROJEKT.firebaseapp.com",
-  projectId: "DEIN_PROJEKT",
-  storageBucket: "DEIN_PROJEKT.appspot.com",
-  messagingSenderId: "DEINE_SENDER_ID",
-  appId: "DEINE_APP_ID"
+apiKey: "AIzaSyDEz_5zme77W5vQfz_KyCxJG1tETjS-7cc",
+authDomain: "at-learning-c4bb4.firebaseapp.com",
+projectId: "at-learning-c4bb4",
+storageBucket: "at-learning-c4bb4.firebasestorage.app",
+messagingSenderId: "450143412430",
+appId: "1:450143412430:web:af2a1b085ac7b5568cf09f",
+measurementId: "G-D7N8TQX6JP"
 };
+
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
