0363a3cfcba2a6e813b7f51eb0036de0ae5c290f
pipboy3003 <rndsmqjndr@privaterelay.appleid.com>
2026-07-16 06:30:19 +0200

    Update firebase-init.js

--- docs/rpg/scripts/firebase-init.js
+++ docs/rpg/scripts/firebase-init.js
@@ -1,12 +1,12 @@
 /*
-[2026-07-16 06:26 CEST] Phase 1 initial erstellt.
-- Firebase Initialisierung per modularer Web-SDK.
-- Reale Projektkonfiguration eingesetzt.
-- Für spätere Realtime-Database-Nutzung vorbereitet.
+[2026-07-16 06:28 CEST] Firebase Setup erweitert.
+- Auth und Realtime Database initialisiert.
+- Phase 1 für Login und spätere Multiplayer-Daten vorbereitet.
 */
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBbYbswfsuyXhgK-t_Whz7sGven7J2fT2w",
@@ -14,12 +14,12 @@ const firebaseConfig = {
   projectId: "rpg01-793eb",
   storageBucket: "rpg01-793eb.firebasestorage.app",
   messagingSenderId: "381934095221",
-  appId: "1:381934095221:web:71007bb26d223de0a7213c"
-  // databaseURL: "https://rpg01-793eb-default-rtdb.europe-west1.firebasedatabase.app"
-  // measurementId: "G-4GCT9YCJJ8"
+  appId: "1:381934095221:web:71007bb26d223de0a7213c",
+  databaseURL: "HIER_DEINE_RTDB_URL_EINTRAGEN"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
 
-export { app, auth };
+export { app, auth, database };
